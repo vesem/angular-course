b326d1cf41a8e31a8f89443fe2bcc2e1fc42ea66
mitch <mitch@nerdfortress.dev>
2020-09-09 12:37:44 -0400

    Component inputs

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Course} from '../model/course';
 
 @Component({
   selector: 'course-card',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseCardComponent implements OnInit {
 
-  constructor() { }
+    @Input()
+    course:Course;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
